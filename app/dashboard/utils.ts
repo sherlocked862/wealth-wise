b662a401261625c078073aef6105517d94122938
Shyam Pipalia <shyampipalia@gmail.com>
2026-01-11 20:31:18 +0530

    fix: Argument of type string | undefined is not assignable to parameter of type string

--- app/dashboard/utils.ts
+++ app/dashboard/utils.ts
@@ -2,7 +2,7 @@
 
 import {GoogleGenerativeAI} from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 export async function scanReceipt(formData: FormData) {
     try{
